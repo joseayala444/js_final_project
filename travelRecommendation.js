5e09534faff620f17dab766b7a538e29d2d0cd5f
Jose Ayala <cccayala39@gmail.com>
2025-02-04 03:38:09 -0500

    java script projecto finar primer commit

---
+++ travelRecommendation.js
@@ -0,0 +1,17 @@
+function contactus() {
+    window.location.href = "contactus.html";
+}
+
+document.getElementById("contactusBtn").addEventListener("click", contactus);
+
+function home() {
+    window.location.href = "travelRecommendation.html";
+}
+
+document.getElementById("homeBtn").addEventListener("click", home);
+
+function aboutus() {
+    window.location.href = "about_us.html";
+}
+
+document.getElementById("aboutusBtn").addEventListener("click", aboutus);
